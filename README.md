e039bc5b366ae791bacc67d393e1ff8a060ba5d7
gosharplite <tony.hsu@droi.com>
2015-12-31 12:46:13 +0800

    v7

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # duck
 Web server as sitting duck.
 
-$ docker build -t gosharplite/duck:v6 .
+$ docker build -t gosharplite/duck:v7 .
 
-$ docker push gosharplite/duck:v6
+$ docker push gosharplite/duck:v7
 
-$ docker run --publish 8093:8092 gosharplite/duck:v6 -port=8092
+$ docker run --publish 8093:8092 gosharplite/duck:v7 -port=8092
--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"time"
 )
 
 var (
-	PORT  = flag.Int("port", 80, "Server port")
-	DELAY = flag.Int("delay", 150, "delay in millisecond")
-	LOGS  = flag.Bool("logs", false, "Show logs")
+	PORT     = flag.Int("port", 80, "Server port")
+	DELAY    = flag.Int("delay", 150, "delay in millisecond")
+	LOGS     = flag.Bool("logs", false, "Show logs")
+	LOCAL_IP = getLocalIP()
 )
 
 func main() {
@@ -33,11 +35,33 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	logs("end")
 
-	fmt.Fprint(w, "quack-"+r.RemoteAddr+"\n")
+	fmt.Fprint(w, "quack - "+r.RemoteAddr+" -> "+LOCAL_IP+"\n")
 }
 
 func logs(format string, v ...interface{}) {
 	if *LOGS {
 		log.Printf(format, v...)
 	}
+}
+
+func getLocalIP() string {
+	ip := "unknown"
+
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		return ip
+	}
+
+	for _, address := range addrs {
+
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ip = ipnet.IP.String()
+				break
+			}
+		}
+	}
+
+	return ip
 }
